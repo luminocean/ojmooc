0f47f57233e7cba63f52a59cd25153187f35b560
luminocean <282896922@qq.com>
2015-01-07 21:43:00 +0800

    Pascal内存统计问题workaround

--- site/ojrunner/shell/docker.sh
+++ site/ojrunner/shell/docker.sh
@@ -26,18 +26,26 @@ echo "$input_data" | (valgrind --tool=massif \
     --time-unit=B --stacks=yes \
     /home/build_repo/"$exe_name") &> /dev/null
 
+#无法生成valgrind报告，可能是valgrind对free pascal生成程序的bug
+#使用GNU/time粗略统计使用内存，即统计RSS数据（max Resident Set Size）
 if [ ! -f "/home/report_repo/${exe_name}_rawmemusg.txt" ];then
-    #无法生成valgrind报告，为valgrind对free pascal生成程序的bug
+    max_rss=$(echo "$input_data" | \
+        (/usr/bin/time -v /home/build_repo/"$exe_name">/dev/null) 2>&1 \
+        | sed -n '10p' | awk '{print $6}')
+    max_rss=$((max_rss*1000))
+    echo "rss $max_rss" >> /home/report_repo/"${exe_name}".txt
     exit 0
 fi
 
+#解析原始数据文件
 ms_print /home/report_repo/${exe_name}_rawmemusg.txt > /home/report_repo/${exe_name}_graph.txt
-
+#从解析结果中取出各个时间点中内存使用最大的值
 max_mem_usage=$(cat /home/report_repo/${exe_name}_graph.txt \
     | grep '^[0-9, ]*$' | sed 's/,//g' \
     | sed '/^\s*$/d' | awk '{print $3}' \
     | awk '{if($1>max)max=$1}END{print max}')
 
+#输出到报告文件中
 echo "mem $max_mem_usage" >> /home/report_repo/"${exe_name}".txt
-
+#清除临时文件
 rm "/home/report_repo/${exe_name}_rawmemusg.txt" "/home/report_repo/${exe_name}_graph.txt"
