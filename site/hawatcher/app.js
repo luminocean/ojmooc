ac9bfab0f06a1b55878d959752559f7b7898e631
luminocean <282896922@qq.com>
2015-01-18 20:23:00 +0800

    HAWatcher使用心跳检测去除未响应的docker容器的负载均衡

--- site/hawatcher/app.js
+++ site/hawatcher/app.js
@@ -14,6 +14,7 @@ setInterval(inspectContainers, 5000);
 
 /*
  * 检查docker内配置的容器是否有变化，如果有变化则刷新HAProxy的负载配置
+ * 将异步的检查动作promise化，所有的docker宿主机的容器列表都取得后统一处理
  */
 function inspectContainers(){
     var promises = [];
@@ -23,10 +24,25 @@ function inspectContainers(){
         var port = config.restful.port;
 
         var url = 'http://'+ip+':'+port+path;
-        promises.push(Q.denodeify(doInspect)(url,ip));
+        promises.push(Q.denodeify(getContainersOnHost)(url,ip));
     }
 
-    Q.all(promises).then(function(containerGroups){
+    Q.allSettled(promises).then(function(results){
+        //每台docker宿主机上配置的docker列表的集合
+        var containerGroups = [];
+
+        //获取每一个promise的结果，如果是fulfilled表示获取成功
+        //将获取的容器组（某一个host上的容器）加入数组中
+        results.forEach(function (result) {
+            if (result.state === "fulfilled") {
+                containerGroups.push(result.value);
+            } else {
+                console.error(result.reason.stack);
+            }
+        });
+
+        //所有宿主机上配置的docker容器集合
+        //仅仅是将containerGroups里面的元素展开而已
         var containers = [];
         for(var i=0; i<containerGroups.length; i++){
             var containerGroup = containerGroups[i];
@@ -36,17 +52,78 @@ function inspectContainers(){
             }
         }
 
-        processContainerChanges(containers);
-    },function(err){
-        console.error(err.stack);
+        validateContainers(containers,function(err,survivors){
+            console.log('共检测到容器数：'+containers.length+' 存活容器数:'+survivors.length);
+            processContainerChanges(survivors);
+        });
     });
 }
 
-function doInspect(url,ip,callback){
+/**
+ * 向docker容器发送心跳检测看看是不是还活着，如果死掉了就把该容器从列表中删除
+ * @param containers 需要检查的容器列表
+ * @param callback
+ */
+function validateContainers(containers,callback){
+    //还存活着的容器，将会作为callback的参数返回
+    var survivors = [];
+    //计数已经检查了几个容器了，全部检查完以后才调用callback
+    var counter = 0;
+
+    for(var i=0; i<containers.length; i++){
+        var container = containers[i];
+        var ip = container.ip;
+
+        if( !container.Ports || !container.Ports[0]
+                || !container.Ports[0].PublicPort){
+            console.error('获得的容器信息中缺少public端口信息：'+JSON.stringify(container));
+            continue;
+        }
+        var publicPort = container.Ports[0].PublicPort;
+        var url = "http://"+ip+':'+publicPort;
+
+        //准备请求的数据
+        var requestObj = {
+            //这里直接访问HAProxy做负载均衡
+            "url":url,
+            "method":"POST",
+            "json":true,
+            "timeout":5000,
+            "body":{
+                "heartBeat":"Are you alive?"
+            }
+        };
+
+        (function(requestObj,container){
+            //发送执行请求，获取执行结果
+            request(requestObj,function(err, response, body){
+                if(err)
+                    return console.error(err.stack);
+                counter++;
+
+                if(body.isAlive)
+                    survivors.push(container);
+
+                if(counter===containers.length){
+                    callback(null,survivors);
+                }
+            });
+        })(requestObj,container);
+    }
+}
+
+/**
+ * 根据指定url获取该地址上docker服务所管理的容器列表
+ * @param url 要访问的docker宿主地址
+ * @param ip url里面的ip部分，为了免去解析就直接传进来了，用来标识某个容器在哪台主机上
+ * @param callback 回调函数
+ */
+function getContainersOnHost(url,ip,callback){
     //准备请求的数据
     var requestObj = {
         "url":url,
-        "method":"GET"
+        "method":"GET",
+        "timeout":5000
     };
     //发送执行请求，获取执行结果
     request(requestObj,function(err, response, body){
