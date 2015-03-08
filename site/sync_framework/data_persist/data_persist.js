ca271e170c7ea46e89ad794629279565ca0cb2a9
zlm11 <2219281845@qq.com>
2015-03-08 15:54:33 +0800

    数据持久化封装

--- site/sync_framework/data_persist/data_persist.js
+++ site/sync_framework/data_persist/data_persist.js
@@ -2,32 +2,31 @@
  * Created by blueking on 2015/3/7.
  */
 var mysql = require('../node_modules/mysql');
+exports.queryInfo = function(tablename,conditon,value) {
+    var database_name = 'ojmooc';
+    var table_name = tablename;
 
-var database_name = 'ojmooc';
-var table_name = 't_course_info';
+    var client = mysql.createConnection({
+        user: 'root',
+        password: '123456'
+    });
 
-var client = mysql.createConnection({
-    user: 'root',
-    password: '123456'
-});
+    client.connect();
+    client.query("use " + database_name);
 
-client.connect();
-client.query("use " + database_name);
-
-client.query(
-    'SELECT * FROM '+table_name,
-    function selectCb(err, results, fields) {
-        if (err) {
-            throw err;
-        }
-
-        if(results)
-        {
-            for(var i = 0; i < results.length; i++)
-            {
-                console.log("%d\t%s\t%s", results[i].cid, results[i].cname,results[i].csection);
+    client.query(
+        'SELECT * FROM '+table_name + ' where ' + conditon + ' = ?', [value],
+        function selectCb(err, results, fields) {
+            if (err) {
+                throw err;
             }
+
+            if (results) {
+                for (var i = 0; i < results.length; i++) {
+                    console.log("%d\t%s\t%s", results[i].cid, results[i].cname, results[i].csection);
+                }
+            }
+            client.end();
         }
-        client.end();
-    }
-);
+    );
+}
---
+++ site/sync_framework/data_persist/test_data_persist.js
@@ -0,0 +1,6 @@
+/**
+ * Created by blueking on 2015/3/8.
+ */
+dp = require('./data_persist');
+
+dp.queryInfo('t_course_info','tid',1);
