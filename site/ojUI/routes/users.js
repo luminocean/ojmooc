1bb81e50b6b0f502fae890ef3ebaae6ebcc89b21
zhangyu <609658265@qq.com>
2015-03-11 17:28:28 +0800

    UI的基本框架

---
+++ site/ojUI/routes/users.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/hello', function(req, res, next) {
+  res.send("hello333332222~"+new Date().toString());
+});
+
+router.get(':username', function(req, res, next) {
+  res.send("hello~"+new Date().toString());
+});
+
+module.exports = router;
