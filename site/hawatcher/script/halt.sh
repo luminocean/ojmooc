64ffd5913381ede2cb12707023d60b5d26f9fc8f
luminocean <282896922@qq.com>
2015-03-12 11:08:49 +0800

    脚本完善

--- site/hawatcher/script/halt.sh
+++ site/hawatcher/script/halt.sh
@@ -2,7 +2,7 @@
 #关闭所有开启的hawatcher和haproxy进程
 
 #运行期文件目录
-runtime_dir="/tmp/hawatcher/runtime"
+runtime_path="/tmp/hawatcher/runtime"
 
 #关闭守护模式
 echo "关闭守护模式..."
@@ -11,13 +11,23 @@ if [ -n "$deamon_ids" ];then
     sudo kill $(ps -e | grep script_deamon | awk '{print $1}')
 fi
 
-#关闭hawatcher进程
-for file in $(ls "$runtime_dir");do
-    pid_file_name=$(echo "$file" | grep watcher)
-    pid_file_path="$runtime_dir/$pid_file_name"
+#依次关闭runtime下所有的进程
+echo "关闭所有hawtcher以及haproxy进程..."
+for file in $(ls "$runtime_path");do
+    pid_file_name=$(echo "$file" | grep 'pid')
+    pid_file_path="$runtime_path/$pid_file_name"
     if [ -n "$pid_file_name" -a -f "$pid_file_path" ];then
-        watcher_pid=$(cat "$runtime_dir/$pid_file_name")
-        #向watcher进程发送信号
-        sudo kill "$watcher_pid"
+        pid=$(cat "$runtime_path/$pid_file_name")
+        if [ -n $(ps -e | grep "$pid" | awk '{print $1}') ];then
+            sudo kill "$pid"
+            sleep 1
+        fi
     fi
+done
+
+#清空runtime下的所有文件
+echo "清理hawatcher相关运行期文件"
+for file in $(ls "$runtime_path");do
+    file_path="$runtime_path/$file"
+    rm "$file_path"
 done
--- site/hawatcher/script/script_deamon.sh
+++ site/hawatcher/script/script_deamon.sh
@@ -4,14 +4,39 @@
 command=$1
 log_file_path=$2
 
-watcher_runtime_path="/tmp/hawatcher/runtime"
+runtime_path="/tmp/hawatcher/runtime"
 
+#清理hawatcher异常退出而得不到清理的haproxy
+function cleanup(){
+    for file in $(ls "$runtime_path");do
+        watcher_pid_file_name=$(echo "$file" | grep 'watcher')
+        watcher_pid_file_path="$runtime_path/$watcher_pid_file_name"
+        if [ -n "$watcher_pid_file_name" -a -f "$watcher_pid_file_path" ];then
+            watcher_pid=$(cat "$runtime_path/$watcher_pid_file_name")
+            #如果hawatcher进程已经挂掉
+            if [ -z $(ps -e | grep "$watcher_pid" | awk '{print $1}') ];then
+                #把watcher的pid文件名中去掉.watcher就得到了haproxy的pid文件名
+                proxy_pid_file_name=${watcher_pid_file_name/\.watcher/''}
+                proxy_pid_file_path="$runtime_path/$proxy_pid_file_name"
+                #把这个挂掉的hawatcher带起来的haproxy也给关掉
+                if [ -n "$proxy_pid_file_name" -a -f "$proxy_pid_file_path" ];then
+                    pid=$(cat "$proxy_pid_file_path")
+                    if [ -n $(ps -e | grep "$pid" | awk '{print $1}') ];then
+                        sudo kill "$pid"
+                        sleep 1
+                    fi
+                fi
+            fi
+        fi
+    done
+}
 
 echo "守护模式开启..."
 while true;
 do
     {
-        #这里要考虑hawatcher意外退出的情况，这时它带起来的haproxy还没有退出，这需要检查出来后进行清理
+        cleanup
+
         ${command}
         echo "Stopped unexpected, restarting \r\n\r\n"
     } &>> "${log_file_path}"
