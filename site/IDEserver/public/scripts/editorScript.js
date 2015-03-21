e1de6b26f8811295c6b14c0d6d4861a25d9ea03a
savioYO <179495657@qq.com>
2015-03-21 22:08:32 +0800

    连接到服务器，实现基本的编译显示

---
+++ site/IDEserver/public/scripts/editorScript.js
@@ -0,0 +1,10 @@
+/**
+ * Created by savio on 2015/3/21.
+ */
+
+editorSrc = "/scripts/aceJS/ace.js";
+aceEditor = document.createElement("script");
+aceEditor.setAttribute("type","text/javascript");
+aceEditor.setAttribute("src",editorSrc);
+document.appendChild(aceEditor);
+
--- site/IDEserver/server.js
+++ site/IDEserver/server.js
@@ -1,19 +1,20 @@
 // server.js
-
 var runner = require('../ojclient/app.js').runner;
-var fs = require('fs');
-
 var express = require('express');
 var app = express();
 var url = require('url');
 var path = require('path');
 
-//引入body-parser，允许从请求中获得参数
-app.use(require('body-parser').urlencoded({extended: true}))
+/**
+ * 引入body-parser模块，允许从请求中获取参数
+ */
+app.use(require('body-parser').urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
-// index page
+/**
+ * index page
+ */
 app.get('/', function(req, res) {
     var drinks = [
         { name: 'Bloody Mary', drunkness: 3 },
@@ -28,32 +29,32 @@ app.get('/', function(req, res) {
     });
 });
 
-// IDE page
 
+/**
+ * IDE page
+ */
 app.get('/IDE', function (req,res) {
     res.render('pages/IDE');
 });
-//    runner.setPort(8080);
-//    runner.setHost('121.42.155.75');
-//    var srcType = 'pas';
-//    //读取测试用源文件
-//    var srcCode = fs.readFileSync('../ojclient/input_data/'+srcType+'_code','utf-8');
-//    //读取测试用数据
-//    var inputData = fs.readFileSync('../ojclient/input_data/'+srcType+'_data','utf-8');
-//        runner.run(srcCode,srcType,inputData,function(err,result,params,host) {
-//            if (err) return console.error(err);
-//
-//            res.render('pages/IDE',{
-//                result:result
-//            })
-//        });
 
+
+/**
+ * 应对客户端的ajax请求
+ */
 app.post('/IDE/handle', function (req,res) {
-    console.log("IN");
-    var content = req.body.Name;
-    console.log(content);
-    res.send(content);
-    res.end();
+    runner.setPort(8080);
+    runner.setHost('121.42.155.75');
+
+    var code = req.body.code;
+    var language = req.body.language;
+    var params = req.body.params;
+
+    runner.run(code,language,params,function(err,result,params,host) {
+        if (err) return console.error(err);
+        res.send(result);
+        res.end();
+    });
 });
+
 app.listen(8080);
 console.log('8080 is the magic port');
