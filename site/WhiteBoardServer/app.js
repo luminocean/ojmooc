48ad7b1851c6aee13aa82a923102ebfe397e9f77
袁斌华 <313442958@qq.com>
2015-04-10 14:05:12 +0800

    白板服务器重构

--- site/WhiteBoardServer/app.js
+++ site/WhiteBoardServer/app.js
@@ -36,7 +36,7 @@ var uploadServer = http.createServer(function(req,res){
                         console.log("err");
                     }
                     else{
-                        res.writeHead(200, { 'Content-Type': 'image/jpeg','Connection': 'close','Access-Control-Allow-Origin':'http://localhost' });
+                        res.writeHead(200, { 'Content-Type': 'image','Connection': 'close','Access-Control-Allow-Origin':'http://localhost' });
                         res.write(data);
                         res.end();
                     }
